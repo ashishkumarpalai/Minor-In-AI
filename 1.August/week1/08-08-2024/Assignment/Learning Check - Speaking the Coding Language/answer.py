94a8615e477a9baf2a241eb84638ed285ac6930d
Ashish kumar palai <ashishkumarpalai2000@gmail.com>
2024-08-15 14:42:14 +0530

    date 08-08-2024 assignment done

---
+++ 1.August/week1/08-08-2024/Assignment/Learning Check - Speaking the Coding Language/answer.py
@@ -0,0 +1,45 @@
+### 1 What is the result of the following expression ?
+
+10%3
+print(10%3) # 1 
+
+
+### 2 What will be the following code Output
+
+x = 1
+x = x + 1
+print(x) # 2 
+
+### 3 What will be the following code Output
+x = 10 
+y = 15
+z = y > x
+print(z) # True 
+
+### 4 What is the result of the following expression ?
+
+10//3
+print(10//3) # 3
+
+### 5 What will be the following code Output 
+
+print("Hello world") # Hello world
+
+### 6 What will be the following code Output
+
+print(2+3*4) # 14
+
+### 7 What will be the following code Output
+
+# print(1) # Nothing print
+
+### 8 What will be the following code Output
+
+print(1, 2) # 1 2
+
+### 9 which operator check equlity
+
+# ==
+
+### 10 What will be the following code Output
+print("Python "*3)
