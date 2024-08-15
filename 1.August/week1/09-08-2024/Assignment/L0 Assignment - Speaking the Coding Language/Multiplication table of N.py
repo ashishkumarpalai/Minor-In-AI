ad77b8985b1af9061b3be89380cefcf778d3d628
Ashish kumar palai <ashishkumarpalai2000@gmail.com>
2024-08-15 14:47:08 +0530

    lo assignment of multiplication of table n

---
+++ 1.August/week1/09-08-2024/Assignment/L0 Assignment - Speaking the Coding Language/Multiplication table of N.py
@@ -0,0 +1,8 @@
+def solve(num):
+   '''You are not supposed to take input.
+      Input value is already given to you
+      as num. You just need to use this value
+      to write down the program.
+   '''
+   for i in range(1,11):
+     print(num*i)
